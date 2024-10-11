5048db32681385a73d8afa0843a57617c5ec00da
Daniel <a20212591@pucp.edu.pe>
2024-10-10 19:20:20 -0500

    Entities

---
+++ lab7_20212591/src/main/java/com/example/lab7_20212591/Lab720212591Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20212591;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720212591Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab720212591Application.class, args);
+	}
+
+}
---
+++ lab7_20212591/src/test/java/com/example/lab7_20212591/Lab720212591ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20212591;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720212591ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
